33895bb7fa1de432526084ee512dddd2e9924829
Kelson-leo <kelsonlean@gmail.com>
2026-05-09 02:56:16 -0300

    feat: add data validation to Movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MoviesAPI.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "The movie title is required")]
     public string Title { get; set; }
 
+    [Required(ErrorMessage = "The movie genre is required")]
+    [MaxLength(50, ErrorMessage = "The genre length cannot exceed 50 characters")]
     public string Genre { get; set; }
 
+    [Required]
+    [Range(70, 600, ErrorMessage = "Duration must be between 70 and 600 minutes")]
     public string Duration { get; set; }
 }
