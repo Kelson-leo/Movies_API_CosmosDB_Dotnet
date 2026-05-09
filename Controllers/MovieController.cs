27c8d3052ca203d04eaee7a0734e5d6853d63ea1
Kelson-leo <kelsonlean@gmail.com>
2026-05-09 02:39:12 -0300

    feat: add movie controller and bruno test collection

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+using MoviesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Duration);
+        Console.WriteLine(movie.Genre);
+    }
+
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+namespace MoviesAPI.Models;
+
+public class Movie
+{
+    public string Title { get; set; }
+
+    public string Genre { get; set; }
+
+    public string Duration { get; set; }
+}
